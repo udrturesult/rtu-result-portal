fe7759e22e02b1f3bc8d18b0bf318a4b6b3eb78a
udrturesult <kjpr658cx6@privaterelay.appleid.com>
2026-03-28 16:14:53 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+const students = [
+
+{
+roll:"RTU21CS001",
+dob:"2000-07-10",
+name:"Rahul Sharma",
+branch:"Computer Science",
+maths:82,
+physics:75,
+programming:89
+},
+
+{
+roll:"RTU21CS002",
+dob:"2001-02-15",
+name:"Aman Jain",
+branch:"Computer Science",
+maths:72,
+physics:68,
+programming:80
+}
+
+];
+
+function checkResult(){
+
+let roll=document.getElementById("roll").value;
+let dob=document.getElementById("dob").value;
+
+let student=students.find(s=>s.roll===roll && s.dob===dob);
+
+let result=document.getElementById("result");
+
+if(student){
+
+result.innerHTML=`
+<h3>${student.name}</h3>
+
+<p>Roll No: ${student.roll}</p>
+
+<p>Branch: ${student.branch}</p>
+
+<table style="width:100%">
+<tr><td>Maths</td><td>${student.maths}</td></tr>
+<tr><td>Physics</td><td>${student.physics}</td></tr>
+<tr><td>Programming</td><td>${student.programming}</td></tr>
+</table>
+
+<p><b>Result: PASS</b></p>
+`;
+
+}
+else{
+
+result.innerHTML="Invalid Roll Number or Date of Birth";
+
+}
+
+}
